f62ffbaa8e239a123f00cc2ec909ffc0c42bf26b
Kristin Jackvony <kristin.jackvony@nextsteplivinginc.com>
2019-03-08 17:34:55 -0500

    Adding in test file

---
+++ easyFreeAccessibilityTest.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get("https://www.amazon.com/s?k=goodnight+moon&ref=nb_sb_noss_1")
+elem = driver.find_element_by_class_name("s-image")
+val = elem.get_attribute('alt')
+if val == 'Goodnight Moon':
+	print('Alt text is present')
+else:
+	print('Alt text is missing')
+
+driver.get("https://www.audi.com/en.html")
+driver.find_element_by_link_text("DE").click()
+try:
+	elem = driver.find_element_by_link_text("Kontakt")
+	if elem:	
+		print('Page is in German')
+except:
+	print('Page is not in German')
+
+driver.quit()
